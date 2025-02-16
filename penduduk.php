f543d0dd0455ce81c0c1834a4be56c9b70e16189
RaflyAmartharizqi <raflyrizqi25@gmail.com>
2025-02-16 18:29:57 +0700

    OK

--- penduduk.php
+++ penduduk.php
@@ -20,7 +20,7 @@
                     <li><a href="profildesa.php">Profil Desa</a></li>
                     <li><a href="Pemerintahan.php">Pemerintahan</a></li>
                     <li><a href="penduduk.php">Data Desa</a></li>
-                    <li><a href="berita.php">Okeh</a></li>
+                    <li><a href="berita.php">Kontol</a></li>
                     <li><a href="login/login.php">Login</a></li>
                     <li><a href="">ygiyighkjbugk</a></li>
                 </ul>
